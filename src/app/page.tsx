2c41f9d241348a82e6be7369a868050812a0f5be
AmirAllahpoor <162034746+AmirAllahpoor@users.noreply.github.com>
2024-06-28 17:57:13 +0330

    New Commit
    Fix Responsive Invesment Container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,11 @@ export default function Home() {
   return (
       <>
        <HeroSection />
-       <div className="flex flex-row justify-center pr-10 lg:pr-0 lg:justify-between lg:flex-col">
-          <div className="flex lg:flex-row flex-col lg:items-center items-start lg:justify-between justify-center xl:px-[130px] lg:px-10 lg:mt-[15rem] mt-14  lg:pb-20 pb-10">
+          <div className="flex flex-wrap gap-x-24 gap-y-7 justify-center xl:px-[130px] lg:px-10 lg:mt-[15rem] mt-14  lg:pb-20 pb-10">
               <InvesmentCard title="شرکت فعال" count="112610"/>
               <InvesmentCard title="میلیارد تومان سرمایه گذاری" count="11"/>
               <InvesmentCard title="سرمایه گذار" count="496"/>
           </div>
-       </div>
        <div className="flex flex-wrap gap-x-10 gap-y-24 justify-center items-center xl:px-[130px] px-10 pb-20 mt-24">
             <InstructionCard SrcIMG="instruction1" Title="شرکت های مورد علاقت رو پیدا کن" Background="bg-[#AECFFE]"/>
             <InstructionCard SrcIMG="instruction2" Title="کسب و کارهای مناسب رو پیدا کن" Background="bg-[#79AEFE]"/>
